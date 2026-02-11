81c9218e3ea215857b95fc4e05c66c967cc613e5
PhilH496 <81784035+PhilH496@users.noreply.github.com>
2026-02-10 23:43:36 -0700

    new func for getting mod ids that are enabled. also added check to exclude mods with missing nexus ids

--- mo2_api_bridge/nexus_display/nexus_display.py
+++ mo2_api_bridge/nexus_display/nexus_display.py
@@ -1,8 +1,8 @@
-import time
 from typing import Dict, List
 from bridge_client import MO2BridgeClient
 
 def getModIds(client: MO2BridgeClient) -> Dict[str, List[int]]:
+    """Gets all mod ids."""
     mod_names = client.call('modList.allMods')
     nexus_ids = []
 
@@ -10,8 +10,26 @@ def getModIds(client: MO2BridgeClient) -> Dict[str, List[int]]:
         for mod_name in mod_names:
             try:
                 nexus_id = client.call('modList.nexusId', mod_name)
-                if nexus_id:
+                if nexus_id and nexus_id != -1:
                     nexus_ids.append(nexus_id)
             except Exception as e:
                 print(f'Error getting Nexus ID for {mod_name}: {e}')
     return {'nexus_ids': nexus_ids}
+
+def getEnabledModIds(client: MO2BridgeClient) -> Dict[str, List[int]]:
+    """Gets only enabled mod ids."""
+    mod_names = client.call('modList.allMods')
+
+    enabled_ids = []
+    if mod_names:
+        for mod_name in mod_names:
+            try:
+                state = client.call('modList.state', mod_name)
+                if state == 35: # 35 is the state for enabled mods
+                    nexus_id = client.call('modList.nexusId', mod_name)
+                    if nexus_id and nexus_id != -1:
+                        enabled_ids.append(nexus_id)
+
+            except Exception as e:
+                print(f'Error getting enabled mod id for {mod_name}: {e}')
+    return {'enabled_ids': enabled_ids}
